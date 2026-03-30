ea1c9c24ecdc020788a163a667f079d6519c5d40
vvalenromero <valengoromero8@gmail.com>
2026-03-30 19:03:05 -0300

    modificacion entrada manual

--- src/factorial/factorial.py
+++ src/factorial/factorial.py
@@ -6,23 +6,10 @@
 #* Creative commons                                                        *
 #*-------------------------------------------------------------------------*
 import sys
-def factorial(num): 
-    if num < 0: 
-        print("Factorial de un número negativo no existe")
-        return 0
-    elif num == 0: 
-        return 1
-        
-    else: 
-        fact = 1
-        while(num > 1): 
-            fact *= num 
-            num -= 1
-        return fact 
 
-if len(sys.argv) == 0:
-   print("Debe informar un número!")
-   sys.exit()
-num=int(sys.argv[1])
-print("Factorial ",num,"! es ", factorial(num)) 
+# [Inferencia] Verificamos si se pasó un argumento
+if len(sys.argv) < 2:
+    entrada = input("No ingresó argumento. Por favor, ingrese el número o rango: ")
+else:
+    entrada = sys.argv[1]
 
